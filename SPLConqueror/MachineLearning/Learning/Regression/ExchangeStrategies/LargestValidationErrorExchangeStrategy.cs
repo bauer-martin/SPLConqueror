2c648adeb604e26ac05398f70fac01db64c9e44f
Martin Bauer <bauer_martin@icloud.com>
2018-10-29 19:21:14 +0100

    Extract super class for exchange strategies that select configurations with largest validation error

---
+++ SPLConqueror/MachineLearning/Learning/Regression/ExchangeStrategies/LargestValidationErrorExchangeStrategy.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SPLConqueror_Core;
+
+namespace MachineLearning.Learning.Regression.ExchangeStrategies
+{
+    public abstract class LargestValidationErrorExchangeStrategy: ConfigurationExchangeStrategy
+    {
+        protected readonly ML_Settings mlSettings;
+        private readonly double percentageToExchange;
+
+        protected LargestValidationErrorExchangeStrategy(ML_Settings mlSettings, double percentageToExchange)
+        {
+            this.mlSettings = mlSettings;
+            this.percentageToExchange = percentageToExchange;
+        }
+
+        public void exchangeConfigurations(List<Configuration> learningSet, List<Configuration> validationSet,
+            List<Feature> model)
+        {
+            // determine how many configurations need to be exchanged
+            int numberOfConfigsToExchange = (int) (validationSet.Count * percentageToExchange);
+
+            // select configurations for exchange
+            IEnumerable<Configuration> learningConfigsToExchange =
+                SelectConfigsFromLearningSet(learningSet, numberOfConfigsToExchange);
+            IEnumerable<Configuration> validationConfigsToExchange =
+                SelectConfigsFromValidationSet(validationSet, numberOfConfigsToExchange, model);
+
+            // exchange configurations
+            learningSet.RemoveAll(config => learningConfigsToExchange.Contains(config));
+            validationSet.RemoveAll(config => validationConfigsToExchange.Contains(config));
+            learningSet.AddRange(validationConfigsToExchange);
+            validationSet.AddRange(learningConfigsToExchange);
+        }
+
+        protected abstract IEnumerable<Configuration> SelectConfigsFromLearningSet(List<Configuration> learningSet,
+            int count);
+
+        private IEnumerable<Configuration> SelectConfigsFromValidationSet(List<Configuration> validationSet, int count,
+            List<Feature> model)
+        {
+            IEnumerable<Configuration> configsSortedByError = SortedConfigsByError(validationSet, model);
+            return configsSortedByError.Take(count);
+        }
+
+        private IEnumerable<Configuration> SortedConfigsByError(List<Configuration> configs, List<Feature> model)
+        {
+            List<Tuple<Configuration, double>> list = new List<Tuple<Configuration, double>>();
+            foreach (Configuration c in configs)
+            {
+                double estimatedValue = FeatureSubsetSelection.estimate(model, c);
+                double realValue = c.GetNFPValue(GlobalState.currentNFP);
+                double error = 0;
+                switch (mlSettings.lossFunction)
+                {
+                    case ML_Settings.LossFunction.RELATIVE:
+                        error = Math.Abs((estimatedValue - realValue) / realValue) * 100;
+                        break;
+                    case ML_Settings.LossFunction.LEASTSQUARES:
+                        error = Math.Pow(realValue - estimatedValue, 2);
+                        break;
+                    case ML_Settings.LossFunction.ABSOLUTE:
+                        error = Math.Abs(realValue - estimatedValue);
+                        break;
+                }
+                list.Add(new Tuple<Configuration, double>(c, error));
+            }
+            return list.OrderByDescending(tuple => tuple.Item2).Select(tuple => tuple.Item1);
+        }
+    }
+}
--- SPLConqueror/MachineLearning/Learning/Regression/ExchangeStrategies/PerformanceValueExchangeStrategy.cs
+++ SPLConqueror/MachineLearning/Learning/Regression/ExchangeStrategies/PerformanceValueExchangeStrategy.cs
@@ -5,7 +5,7 @@ using SPLConqueror_Core;
 
 namespace MachineLearning.Learning.Regression.ExchangeStrategies
 {
-    public class PerformanceValueExchangeStrategy : ConfigurationExchangeStrategy
+    public class PerformanceValueExchangeStrategy : LargestValidationErrorExchangeStrategy
     {
         private class Distribution
         {
@@ -105,30 +105,11 @@ namespace MachineLearning.Learning.Regression.ExchangeStrategies
             }
         }
 
-        private readonly ML_Settings mlSettings;
-
-        public PerformanceValueExchangeStrategy(ML_Settings mlSettings) { this.mlSettings = mlSettings; }
-
-        public void exchangeConfigurations(List<Configuration> learningSet, List<Configuration> validationSet,
-            List<Feature> model)
+        public PerformanceValueExchangeStrategy(ML_Settings mlSettings) : base(mlSettings, 0.1)
         {
-            // determine how many configurations need to be exchanged
-            int numberOfConfigsToExchange = (int) (validationSet.Count * 0.1);
-
-            // select configurations for exchange
-            IEnumerable<Configuration> learningConfigsToExchange =
-                SelectConfigsFromLearningSet(learningSet, numberOfConfigsToExchange);
-            IEnumerable<Configuration> validationConfigsToExchange =
-                SelectConfigsFromValidationSet(validationSet, numberOfConfigsToExchange, model);
-
-            // exchange configurations
-            learningSet.RemoveAll(config => learningConfigsToExchange.Contains(config));
-            validationSet.RemoveAll(config => validationConfigsToExchange.Contains(config));
-            learningSet.AddRange(validationConfigsToExchange);
-            validationSet.AddRange(learningConfigsToExchange);
         }
 
-        private static IEnumerable<Configuration> SelectConfigsFromLearningSet(List<Configuration> learningSet,
+        protected override IEnumerable<Configuration> SelectConfigsFromLearningSet(List<Configuration> learningSet,
             int count)
         {
             List<Configuration> mutableConfigsList = new List<Configuration>(learningSet);
@@ -151,39 +132,6 @@ namespace MachineLearning.Learning.Regression.ExchangeStrategies
                 }
             } while (selectedConfigs.Count < count && mutableConfigsList.Count > 0);
             return selectedConfigs.Take(count).ToList();
-        }
-
-        private IEnumerable<Configuration> SelectConfigsFromValidationSet(List<Configuration> validationSet, int count,
-            List<Feature> model)
-        {
-            IEnumerable<Configuration> configsSortedByError = SortedConfigsByError(validationSet, model);
-            return configsSortedByError.Take(count);
-        }
-
-        private IEnumerable<Configuration> SortedConfigsByError(List<Configuration> configs, List<Feature> model)
-        {
-            List<Tuple<Configuration, double>> list = new List<Tuple<Configuration, double>>();
-            foreach (Configuration c in configs)
-            {
-                double estimatedValue = FeatureSubsetSelection.estimate(model, c);
-                double realValue = c.GetNFPValue(GlobalState.currentNFP);
-                double error = 0;
-                switch (mlSettings.lossFunction)
-                {
-                    case ML_Settings.LossFunction.RELATIVE:
-                        error = Math.Abs((estimatedValue - realValue) / realValue) * 100;
-                        break;
-                    case ML_Settings.LossFunction.LEASTSQUARES:
-                        error = Math.Pow(realValue - estimatedValue, 2);
-                        break;
-                    case ML_Settings.LossFunction.ABSOLUTE:
-                        error = Math.Abs(realValue - estimatedValue);
-                        break;
-                }
-                list.Add(new Tuple<Configuration, double>(c, error));
-            }
-            list.Sort((tuple1, tuple2) => -tuple1.Item2.CompareTo(tuple2.Item2));
-            return list.Select(tuple => tuple.Item1).ToList();
         }
     }
 }
