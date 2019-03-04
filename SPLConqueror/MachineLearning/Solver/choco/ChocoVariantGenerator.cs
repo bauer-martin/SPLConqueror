09c8bf6d027c4b4a558ceedea3495166c43a033b
Martin Bauer <bauer_martin@icloud.com>
2019-03-04 17:11:53 +0100

    Implement FindConfig

--- SPLConqueror/MachineLearning/Solver/choco/ChocoVariantGenerator.cs
+++ SPLConqueror/MachineLearning/Solver/choco/ChocoVariantGenerator.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using SPLConqueror_Core;
 
 namespace MachineLearning.Solver
@@ -9,6 +10,21 @@ namespace MachineLearning.Solver
         private readonly JavaSolverAdapter _adapter;
 
         public ChocoVariantGenerator(JavaSolverAdapter adapter) { _adapter = adapter; }
+
+        private static List<BinaryOption> ParseBinaryOptions(string str, VariabilityModel vm)
+        {
+            List<BinaryOption> result;
+            if (str.Equals("none"))
+            {
+                result = null;
+            }
+            else
+            {
+                string[] tokens = str.Split(',');
+                result = tokens.Select(vm.getBinaryOption).ToList();
+            }
+            return result;
+        }
 
         public List<List<BinaryOption>> DistanceMaximization(VariabilityModel vm,
             List<BinaryOption> minimalConfiguration, int numberToSample, int optionWeight)
@@ -34,7 +50,23 @@ namespace MachineLearning.Solver
         public List<BinaryOption> FindConfig(List<BinaryOption> config, VariabilityModel vm, bool minimize,
             List<BinaryOption> unWantedOptions)
         {
-            throw new NotImplementedException();
+            _adapter.LoadVm(vm);
+            _adapter.SetSolver(SolverType.CHOCO);
+            string optionsString = String.Join(",", config.Select(o => o.Name));
+            string optimizationString = minimize ? "minimize" : "maximize";
+            string response;
+            if (unWantedOptions == null)
+            {
+                response = _adapter.Execute($"find-optimal-config {optimizationString} {optionsString}");
+            }
+            else
+            {
+                string unwantedOptionsString = String.Join(",", unWantedOptions.Select(o => o.Name));
+                response = _adapter.Execute(
+                    $"find-optimal-config {optimizationString} {optionsString} {unwantedOptionsString}");
+            }
+            List<BinaryOption> optimalConfig = ParseBinaryOptions(response, vm);
+            return optimalConfig;
         }
 
         public List<List<BinaryOption>> FindAllConfigs(List<BinaryOption> config, VariabilityModel vm, bool minimize,
