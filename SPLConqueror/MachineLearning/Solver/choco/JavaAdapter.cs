b8d4aa2e480865f3a91a6c9c007e27f7d2851929
Martin Bauer <bauer_martin@icloud.com>
2019-03-04 13:07:23 +0100

    Log error when jar execution terminates unexpectedly

--- SPLConqueror/MachineLearning/Solver/choco/JavaAdapter.cs
+++ SPLConqueror/MachineLearning/Solver/choco/JavaAdapter.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using SPLConqueror_Core;
 
 namespace MachineLearning.Solver
 {
@@ -9,6 +10,7 @@ namespace MachineLearning.Solver
         private Process _javaProcess;
         private readonly string _pathToJar;
         private StreamReader _javaOutput;
+        private StreamReader _javaError;
         private StreamWriter _javaInput;
 
         internal JavaAdapter(string pathToJar) { this._pathToJar = pathToJar; }
@@ -23,18 +25,27 @@ namespace MachineLearning.Solver
                     Arguments = $"-jar {_pathToJar}",
                     UseShellExecute = false,
                     RedirectStandardOutput = true,
+                    RedirectStandardError = true,
                     RedirectStandardInput = true
                 }
             };
             _javaProcess.Start();
             _javaOutput = _javaProcess.StandardOutput;
+            _javaError = _javaProcess.StandardError;
             _javaInput = _javaProcess.StandardInput;
         }
 
         internal string Execute(String command)
         {
             _javaInput.WriteLine(command);
-            return _javaOutput.ReadLine();
+            string response = _javaOutput.ReadLine();
+            if (response == null)
+            {
+                string errorMessage = _javaError.ReadToEnd();
+                GlobalState.logError.logLine(errorMessage);
+                throw new JavaException("Jar execution terminated; see error log.");
+            }
+            return response;
         }
 
         internal void Terminate()
