da86d5e96dfac4497351ce9ade072cca84988baa
Martin Bauer <bauer_martin@icloud.com>
2019-04-09 13:23:49 +0200

    Sort options case-insensitively

--- SPLConqueror/MachineLearning/Solver/external/VariabilityModelIndexOptionCoding.cs
+++ SPLConqueror/MachineLearning/Solver/external/VariabilityModelIndexOptionCoding.cs
@@ -16,7 +16,9 @@ namespace MachineLearning.Solver
         {
             if (vm.Name == _variabilityModelName) return;
             _variabilityModelName = vm.Name;
-            List<BinaryOption> binaryOptions = vm.BinaryOptions.OrderBy(option => option.Name).ToList();
+            Comparer<string> comparer
+                = Comparer<string>.Create((x, y) => string.Compare(x, y, StringComparison.InvariantCultureIgnoreCase));
+            List<BinaryOption> binaryOptions = vm.BinaryOptions.OrderBy(option => option.Name, comparer).ToList();
             _encodingSubstitutions = new Dictionary<string, string>(binaryOptions.Count);
             _decodingSubstitutions = new Dictionary<string, string>(binaryOptions.Count);
             for (int i = 0; i < binaryOptions.Count; i++)
